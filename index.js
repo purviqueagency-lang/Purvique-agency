b54d0ce7d76c4175356d61a6f4005a3deb06311f
purviqueagency-lang <purviqueagency@gmail.com>
2026-08-07 10:17:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
+
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const commandHandler = require("./handlers/commandHandler");
+const eventHandler = require("./handlers/eventHandler");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
   ],
 });
 
-client.once("ready", () => {
-  console.log(`✅ ${client.user.tag} is online!`);
-});
+client.commands = new Collection();
+
+commandHandler(client);
+eventHandler(client);
 
 client.login(process.env.TOKEN);
